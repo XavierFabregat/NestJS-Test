7c0b83b85888a183956cb3bb43264be8d4e07edb
XavierFabregat <xavi.fabregat.pous@gmail.com>
2023-10-25 14:49:35 +0200

    feat: add test for app module

---
+++ src/app/app.module.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '../auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UserModule } from '../user/user.module';
+import { TaskModule } from '../task/Task.module';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './service/app.service';
+import { rootResolver } from '../graphql/resolver/root.resolver';
+
+describe('AppModule', () => {
+  let appModule: TestingModule;
+
+  beforeEach(async () => {
+    appModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+  });
+
+  it('should be defined w/ all the correct import, providers and controllers', () => {
+    expect(appModule).toBeDefined();
+    expect(appModule.get<ConfigModule>(ConfigModule)).toBeDefined();
+    expect(appModule.get<AuthModule>(AuthModule)).toBeDefined();
+    expect(appModule.get<GraphQLModule>(GraphQLModule)).toBeDefined();
+    expect(appModule.get<GraphQLModule>(GraphQLModule)).toBeDefined();
+    expect(appModule.get<UserModule>(UserModule)).toBeDefined();
+    expect(appModule.get<TaskModule>(TaskModule)).toBeDefined();
+    expect(appModule.get<AppController>(AppController)).toBeDefined();
+    expect(appModule.get<AppService>(AppService)).toBeDefined();
+    expect(appModule.get<rootResolver>(rootResolver)).toBeDefined();
+  });
+});
