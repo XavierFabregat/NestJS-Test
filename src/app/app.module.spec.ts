e8c141e38879fe2dc28a446a0b9df0f8e485665a
XavierFabregat <xavi.fabregat.pous@gmail.com>
2023-10-25 16:48:01 +0200

    Seb is a legend

--- src/app/app.module.spec.ts
+++ src/app/app.module.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from './app.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '../auth/auth.module';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -8,6 +7,8 @@ import { TaskModule } from '../task/Task.module';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './service/app.service';
 import { RootResolver } from '../graphql/resolver/root.resolver';
+import { ApolloDriver } from '@nestjs/apollo';
+import { AppModule } from './app.module';
 
 describe('AppModule', () => {
   let appModule: TestingModule;
@@ -22,8 +23,16 @@ describe('AppModule', () => {
     expect(appModule).toBeDefined();
     expect(appModule.get<ConfigModule>(ConfigModule)).toBeDefined();
     expect(appModule.get<AuthModule>(AuthModule)).toBeDefined();
-    expect(appModule.get<GraphQLModule>(GraphQLModule)).toBeDefined();
-    expect(appModule.get<GraphQLModule>(GraphQLModule)).toBeDefined();
+
+    const gqlModule = appModule.get<GraphQLModule>(GraphQLModule);
+    expect(gqlModule).toBeDefined();
+    // @ts-expect-error test needs to verify config via private property
+    expect(gqlModule.options.driver).toEqual(ApolloDriver);
+    // @ts-expect-error test needs to verify config via private property
+    expect(gqlModule.options.autoSchemaFile).toBe('src/graphql/schema.gql');
+    // @ts-expect-error test needs to verify config via private property
+    expect(gqlModule.options.context({ req: 'test' })).toEqual({ req: 'test' });
+
     expect(appModule.get<UserModule>(UserModule)).toBeDefined();
     expect(appModule.get<TaskModule>(TaskModule)).toBeDefined();
     expect(appModule.get<AppController>(AppController)).toBeDefined();
